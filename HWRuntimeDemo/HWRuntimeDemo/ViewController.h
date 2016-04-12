1b4ae7e9b3b7d82074798faa362145ea2bc36d29
Evan <10089084@qq.com>
2016-04-12 21:17:24 +0800

    Added files via upload

---
+++ HWRuntimeDemo/HWRuntimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWRuntimeDemo
+//
+//  Created by Evan on 16/4/12.
+//  Copyright © 2016年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HWRuntimeDemo/HWRuntimeDemo/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  HWRuntimeDemo
+//
+//  Created by Evan on 16/4/12.
+//  Copyright © 2016年 Evan. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "HWMsgSend.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [HWMsgSend testMsgSend];
+}
+
+
+
+@end
