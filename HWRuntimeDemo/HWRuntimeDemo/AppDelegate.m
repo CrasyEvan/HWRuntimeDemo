87e3eb2c27fb668741ea94059e4300408f62f073
huwen824 <huwen824@pingan.com.cn>
2016-04-20 12:28:03 +0800

    etstttss

--- HWRuntimeDemo/HWRuntimeDemo/AppDelegate.m
+++ HWRuntimeDemo/HWRuntimeDemo/AppDelegate.m
@@ -16,9 +16,23 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     return YES;
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
 
 @end
